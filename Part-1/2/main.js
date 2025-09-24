37c34e01dceda03fb1ffa86835a30047c91c8e3b
Kaji Thebe <kajithebe2000@gmail.com>
2025-09-24 22:37:26 +0300

    Updated solution for problem 2 of part-1

--- Part-1/2/main.js
+++ Part-1/2/main.js
@@ -1,12 +1,20 @@
 'use strict';
 
-const x1 = parseFloat(prompt('Enter X coordinate of Point 1:')) || 0;
-const y1 = parseFloat(prompt('Enter Y coordinate of Point 1:')) || 0;
-const x2 = parseFloat(prompt('Enter X coordinate of Point 2:')) || 0;
-const y2 = parseFloat(prompt('Enter Y coordinate of Point 2:')) || 0;
+const coordinate = prompt(
+  'Enter X and Y coordinates of two points (separated by space):'
+);
 
-const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+if (coordinate) {
+  const points = coordinate.split(' ');
 
-document.querySelector('#point1').textContent = `(${x1}, ${y1})`;
-document.querySelector('#point2').textContent = `(${x2}, ${y2})`;
-document.querySelector('#distance').textContent = `${distance}`;
+  const x1 = parseInt(points[0]) || 0;
+  const y1 = parseInt(points[1]) || 0;
+  const x2 = parseInt(points[2]) || 0;
+  const y2 = parseInt(points[3]) || 0;
+
+  const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+  document.querySelector('#point1').textContent = `(${x1}, ${y1})`;
+  document.querySelector('#point2').textContent = `(${x2}, ${y2})`;
+  document.querySelector('#distance').textContent = `${distance}`;
+}
--- Part-1/6/main.js
+++ Part-1/6/main.js
@@ -4,17 +4,24 @@ if (num <= 0) {
   alert('Please enter a positive number greater than 0.');
 } else {
   const tableContainer = document.querySelector('.container table');
+
+  // Clear any content existing in the table, if any
   tableContainer.innerHTML = '';
 
   for (let i = 1; i <= num; i++) {
+    //Add row for each i
     const row = document.createElement('tr');
 
     for (let j = 1; j <= num; j++) {
+      // Inside each row, create j number of cells
       let cell = document.createElement('td');
       cell.innerText = i * j;
+
+      // For each j, append the cell to row
       row.appendChild(cell);
     }
 
+    // Apend all the rows to the table
     tableContainer.appendChild(row);
   }
 }
